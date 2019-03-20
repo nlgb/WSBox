ae5d6f35fb9833481f47aeeacf03a88eddd295e4
wangsong <wanggyanmo@163.com>
2019-03-21 00:31:30 +0800

    增加16进制颜色宏

--- VVSBox/Classes/Macro/ColorHeader.h
+++ VVSBox/Classes/Macro/ColorHeader.h
@@ -10,9 +10,10 @@
 #define ColorHeader_h
 
 // 颜色
-#define COLOR(r,g,b) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:1.0]
+#define RGB(r,g,b) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:1.0]
+#define RGBA(r,g,b,a) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:a]
 
-#define COLOR_WITH_ALPHA(r,g,b,a) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:a]
-
+#define HEXRGB(hex) [UIColor colorWithRed:((float)((hex & 0xFF0000) >> 16)) / 255.0 green:((float)((hex & 0xFF00) >> 8)) / 255.0 blue:((float)(hex & 0xFF)) / 255.0 alpha:1]
+#define HEXRGBA(hex,a) [UIColor colorWithRed:((float)((hex & 0xFF0000) >> 16)) / 255.0 green:((float)((hex & 0xFF00) >> 8)) / 255.0 blue:((float)(hex & 0xFF)) / 255.0 alpha:a]
 
 #endif /* ColorHeader_h */
