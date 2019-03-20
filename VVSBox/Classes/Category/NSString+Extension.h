292ab99041bf9dcaab6a60d44cdbd30ec58de557
wangsong18 <wangsong18@meituan.com>
2019-03-20 10:54:46 +0800

    增加对中文URL的编解码

--- VVSBox/Classes/Category/NSString+Extension.h
+++ VVSBox/Classes/Category/NSString+Extension.h
@@ -28,4 +28,22 @@
  */
 + (NSString *)getCachesPathWithFileName:(NSString *)fileName;
 
+#pragma mark - URL编码解码
+/**
+ *  将一个URL字符串的参数解析到一个字典并返回
+ *  decode：是否需要解码
+ *  @return 返回一个包含URL所有参数的字典
+ */
+- (NSDictionary *)ws_dictionaryByParseInURLParameterFormatWithDecoding:(BOOL)decode;
+/**
+ *  将一个URL字符串解码
+ *  @return 返回一个解码后的URL字符串
+ */
+- (NSString *)ws_urlDecode;
+/**
+ *  将一个URL字符串编码
+ *  @return 返回一个编码后的URL字符串
+ */
+- (NSString *)ws_urlEncode;
+
 @end
